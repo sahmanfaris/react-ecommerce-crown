69cd2521c6e03dcdc886348b06e169fca1670ccb
unknown <farissahman@yahoo.com>
2021-10-12 14:55:56 +0200

    structuring app

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import Directory from '../../components/Directory/Directory'
+import './Homepage.styles.scss'
+
+const Homepage = () => {
+  return (
+    <div className='homepage'>
+      <Directory />
+    </div>
+  )
+}
+
+export default Homepage
