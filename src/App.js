8ae78e926b9f5d8e9a4f59dcea3ed2ec15515134
unknown <farissahman@yahoo.com>
2021-10-14 07:38:27 +0200

    added header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import { Switch, Route, Link } from "react-router-dom";
-import Homepage from "./pages/homepage/Homepage";
-import "./App.css";
-import ShopPage from "./pages/shop/ShopPage";
+import { Switch, Route, Link } from 'react-router-dom'
+import Homepage from './pages/homepage/Homepage'
+import './App.css'
+import ShopPage from './pages/shop/ShopPage'
+import Header from './components/header/Header'
 
 function App() {
   return (
     <>
+      <Header />
       <Switch>
-        <Route exact path="/">
+        <Route exact path='/'>
           <Homepage />
         </Route>
-        <Route path="/shop">
+        <Route path='/shop'>
+          <ShopPage />
+        </Route>
+        <Route path='/contact'>
           <ShopPage />
         </Route>
       </Switch>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
